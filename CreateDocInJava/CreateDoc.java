0a5a3891c09453b773d8104e5144180d5a9bf39d
Salah <bhurgrisalah@gmail.oom>
2024-12-01 12:15:28 +0500

    first Java Practive

---
+++ CreateDocInJava/CreateDoc.java
@@ -0,0 +1,19 @@
+/**
+ * This is a class that demonstrates JavaDoc usage.
+ * 
+ * @author YourName
+ * @version 1.0
+ */
+public class CreateDoc {
+
+    /**
+     * This method adds two numbers.
+     *
+     * @param a the first number
+     * @param b the second number
+     * @return the sum of a and b
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
